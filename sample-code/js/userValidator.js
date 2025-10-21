850a8528bf46c8b5025dbc8b41c8ba34af9d3ebc
Vishal <vishal@biboxlabs.com>
2025-10-21 19:45:36 +0530

    Initial project setup

---
+++ sample-code/js/userValidator.js
@@ -0,0 +1,5 @@
+function validateUser(user) {
+    if (!user.name || !user.email) throw new Error('Invalid user data');
+    return true;
+}
+console.log(validateUser({ name: 'Surabhi', email: 'test@example.com' }));
