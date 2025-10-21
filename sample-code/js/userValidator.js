a29a0eb8e52bc22d4af432a8a9b8022b31c7fb57
Vishal <vishal@biboxlabs.com>
2025-10-21 19:54:18 +0530

    Dummy PR to trigger sanity check

--- sample-code/js/userValidator.js
+++ sample-code/js/userValidator.js
@@ -1,7 +1,2 @@
-function validateUser(user) {
-    if (!user.name || !user.email) throw new Error('Invalid user data');
-    return true;
-}
-console.log(validateUser({ name: 'Surabhi', email: 'test@example.com' }));
-echo "// test PR" >> sample-code/js/test.js
+echo "// test PR" >> sample-code/js/userValidator.js
 
